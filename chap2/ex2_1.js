cc58d86f5202d6483676f19c16f3d5feccb41167
ITbo-com <inbo2002@naver.com>
2023-10-11 02:00:04 +0900

    up

---
+++ chap2/ex2_1.js
@@ -0,0 +1,22 @@
+const factorial = number => {
+    let result = 1;
+    for (let i = 0; i < number; i++) result *= (i + 1);
+    return result;
+    };
+
+const permutation = (n,r) => factorial(n) / factorial(n - r);
+
+const combination = (n,r) => permutation(n, r) / factorial(r);
+
+const multiPermutation = (n,r) =>  n ** r;
+
+const multiCombination = (n,r) => combination(n + r - 1, r);
+
+
+
+module.exports = {
+permutation: permutation,
+combination: combination,
+multiPermutation: multiPermutation,
+multiCombination: multiCombination
+}
